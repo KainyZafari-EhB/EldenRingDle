6034268b5231f60d2a02733d9c3d04ef30449d35
Kainy Zafari <kainy.zafari@student.ehb.be>
2026-03-10 19:31:25 +0100

    fix: corrected fake quotes

--- src/data/quotes.js
+++ src/data/quotes.js
@@ -3,6 +3,7 @@ export const quotes = [
     { id: 1, characterId: 1, text: "I am [...], Blade of Miquella. And I have never known defeat." },
     { id: 2, characterId: 1, text: "Let your flesh be consumed. By the scarlet rot." },
     { id: 3, characterId: 1, text: "You will witness true horror. Now, rot!" },
+    { id: 43, characterId: 1, text: "Wait. The scarlet bloom flowers once more." },
 
     // Margit (id: 2)
     { id: 4, characterId: 2, text: "Put these foolish ambitions to rest." },
@@ -11,16 +12,18 @@ export const quotes = [
 
     // Ranni (id: 3)
     { id: 7, characterId: 3, text: "I stole a fragment of the Rune of Death, and used it to forge the godslaying black knives through fearsome rite." },
-    { id: 8, characterId: 3, text: "I do love thee. For all that thou hast done. Eternally." },
+    { id: 8, characterId: 3, text: "Tell Blaidd, and Iji... I love them." },
     { id: 9, characterId: 3, text: "Mine will be an order not of gold, but the stars and moon of the chill night." },
 
     // Blaidd (id: 4)
     { id: 10, characterId: 4, text: "What a sick way to fight, eh? The glory of the clash is shared, by Radahn and you." },
     { id: 11, characterId: 4, text: "I'm her blade, remember. I'm her shadow. Her... very being." },
+    { id: 44, characterId: 4, text: "My only purpose is to fight for Ranni's fate, sword and fang." },
 
     // Messmer (id: 6)
     { id: 12, characterId: 6, text: "Those stripped of the Grace of Gold shall all meet death. In the embrace of [...]'s flame." },
     { id: 13, characterId: 6, text: "Mother... would you truly lordship deny me?" },
+    { id: 45, characterId: 6, text: "O lightless creature... Embrace thine oblivion, as shall I." },
 
     // Godrick (id: 8)
     { id: 14, characterId: 8, text: "I command thee, kneel! I am the lord of all that is golden!" },
@@ -37,39 +40,47 @@ export const quotes = [
     { id: 21, characterId: 10, text: "Ah, I knew you'd come. To stand before the Elden Ring. To become Elden Lord. What a sad state of affairs." },
     { id: 22, characterId: 10, text: "A Tarnished cannot become a Lord. Not even you." },
     { id: 23, characterId: 10, text: "I am the all-knowing. Did you think I'd sit idle?" },
+    { id: 46, characterId: 10, text: "A man cannot kill a god..." },
+    { id: 47, characterId: 10, text: "Queen Marika has high hopes for us. That we continue to struggle. Unto eternity." },
 
     // Morgott (id: 11)
     { id: 24, characterId: 11, text: "Have it writ upon thy meagre grave: Felled by King [...]! Last of all kings." },
-    { id: 25, characterId: 11, text: "The Erdtree wards off all who deign approach. We are... its defense." },
+    { id: 25, characterId: 11, text: "The Erdtree wards off all who deign approach. We are... we are all forsaken." },
     { id: 26, characterId: 11, text: "Thy kind are all of a piece. Pillagers. Emboldened by the flame of ambition." },
+    { id: 48, characterId: 11, text: "Graceless Tarnished. What is thy business with these thrones?" },
 
     // Mohg (id: 12)
     { id: 27, characterId: 12, text: "Tres! Duo! Unus! Nihil! Nihil! Nihil!" },
     { id: 28, characterId: 12, text: "Welcome, honored guest. To the birthplace of our dynasty!" },
-    { id: 29, characterId: 12, text: "Rise now, ye Tarnished! Ye dead, who yet live!" },
+    { id: 29, characterId: 12, text: "Dearest Miquella. You must abide alone a while." },
 
     // Rykard (id: 13)
     { id: 30, characterId: 13, text: "Join the Serpent King, as family... Together, we will devour the very gods!" },
     { id: 31, characterId: 13, text: "Now, we can devour the gods, togethaaaa!" },
+    { id: 49, characterId: 13, text: "A serpent never dies. Ha ha ha..." },
 
     // Rennala (id: 14)
-    { id: 32, characterId: 14, text: "Sweeting, here is thy rebirth. Now, thy life is mine." },
-    { id: 33, characterId: 14, text: "Upon my name as Queen of the Full Moon... have mercy upon thy children, showered in light." },
+    { id: 32, characterId: 14, text: "Are you ready? For your rebirth." },
+    { id: 33, characterId: 14, text: "Come, sweetings, time to be born, anew." },
+    { id: 50, characterId: 14, text: "Where did ye flee, my sweetings? Come out from whence ye hide." },
 
     // Maliketh (id: 17)
     { id: 34, characterId: 17, text: "O, Death. Become my blade once more." },
     { id: 35, characterId: 17, text: "I will not have it stolen from me again!" },
+    { id: 51, characterId: 17, text: "Cower. Before [...], the Black Blade." },
+    { id: 52, characterId: 17, text: "Witless Tarnished... Why covet Destined Death? To kill what?" },
 
     // Godfrey / Hoarah Loux (id: 18, 19)
     { id: 36, characterId: 18, text: "I have given thee courtesy enough. Now I fight as [...]! Warrior!" },
     { id: 37, characterId: 19, text: "Brave Tarnished... Thy strength befits a crown." },
     { id: 38, characterId: 18, text: "A crown is warranted with strength." },
+    { id: 53, characterId: 18, text: "It's been a long while, Morgott. Long and hard didst thou fight." },
 
     // Miquella (id: 25)
-    { id: 39, characterId: 25, text: "I yield to none. I am [...] of the Haligtree." },
+    { id: 39, characterId: 25, text: "Lord brother. I'm going to be a god. If we keep our part of the vow, promise me you'll be my consort." },
     { id: 40, characterId: 25, text: "Let us go together, my dear consort. O, blessed golden light... take me to thy side." },
 
     // Midra (id: 27)
     { id: 41, characterId: 27, text: "The depths of your foolishness!" },
-    { id: 42, characterId: 27, text: "Shabriri! Have you no shame?" },
+    { id: 42, characterId: 27, text: "Enough. I have endured... more than enough." },
 ];
